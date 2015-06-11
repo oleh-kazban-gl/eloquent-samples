5ae7c5ea14c0df9bf5678fe3d01ea3084c78bb17
Oleh Kazban <oleh.kazban@globallogic.com>
2015-06-11 10:50:54 +0300

    Add exercises from Chapter 3 (Functions)

--- 02 Program Structure/ChessBoard.js
+++ 02 Program Structure/ChessBoard.js
@@ -19,6 +19,8 @@ When you have a program that generates this pattern, define a variable
 size = 8 and change the program so that it works for any size, outputting
 a grid of the given width and height.*/
 
+'use strict';
+
 var dimension = 8;
 var row = '';
 
