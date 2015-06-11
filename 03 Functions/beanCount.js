caffa925f83823c98452308a5782a7ae273bee38
Oleh Kazban <oleh.kazban@globallogic.com>
2015-06-11 10:52:30 +0300

    Add exercises from Chapter #03 - Functions

---
+++ 03 Functions/beanCount.js
@@ -0,0 +1,43 @@
+/*Bean counting
+ You can get the Nth character, or letter, from a string by writing
+ "string".charAt(N), similar to how you get its length with "s".length. The
+ returned value will be a string containing only one character
+ (for example, "b"). The first character has position zero, which causes the
+ last one to be found at position string.length - 1. In other words, a
+ two-character string has length 2, and its characters have positions 0 and 1.
+
+ Write a function countBs that takes a string as its only argument and returns
+ a number that indicates how many uppercase “B” characters are in the string.
+
+ Next, write a function called countChar that behaves like countBs, except it
+ takes a second argument that indicates the character that is to be counted
+ (rather than counting only uppercase “B” characters). Rewrite countBs to make
+ use of this new function.*/
+
+'use strict';
+
+function countBs(inputString) {
+  var characters = inputString.split('');
+  var charCount = 0;
+
+  for (var count = 0; count < characters.length; count++) {
+    if (characters[count] === 'B') {
+      charCount++;
+    }
+  }
+
+  return charCount;
+}
+
+function countChar(inputString, char) {
+  var characters = inputString.split('');
+  var charCount = 0;
+
+  for (var count = 0; count < characters.length; count++) {
+    if (characters[count] === char) {
+      charCount++;
+    }
+  }
+
+  return charCount;
+}
---
+++ 03 Functions/min.js
@@ -0,0 +1,18 @@
+/* Minimum
+
+The previous chapter introduced the standard function Math.min that returns
+its smallest argument. We can do that ourselves now. Write a function min
+that takes two arguments and returns their minimum.
+
+// Your code here.
+console.log(min(0, 10));
+// → 0
+console.log(min(0, -10));
+// → -10
+*/
+
+'use strict';
+
+function min(arg1, arg2) {
+  return (arg1 < arg2 ? arg1 : arg2);
+}
